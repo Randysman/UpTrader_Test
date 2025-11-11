ae95ee7dc53736f48be3f9c287796ef588f4aac0
Randysman <134296647+Randysman@users.noreply.github.com>
2025-11-11 23:15:13 +0300

    create view and path

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,6 @@ from main import views
 
 app_name = 'main'
 
-
 urlpatterns = [
-
+    path("<slug:slug>/", views.menu_page, name='menu_page'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpRequest, HttpResponse
+from .models import MenuItem
 
-# Create your views here.
+
+def menu_page(request: HttpRequest, slug: str) -> HttpResponse:
+    item = get_object_or_404(MenuItem, slug=slug)
+    return render(request, 'page.html', {'item': item})
